374a86ff2af8e68b70b55ab15b331a5f025d2534
舒航 <673545134@qq.com>
2022-09-20 17:30:45 +0800

    feat: demo

---
+++ pages/demo/demo.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="title">
+		<view class="fs-16">
+			demo--fs-16
+		</view>
+		<view class="fs-26">
+			demo--fs-2
+		</view>
+		<view class="fs-30">
+			demo--fs-30
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.title{
+	color: orange;
+}
+</style>
