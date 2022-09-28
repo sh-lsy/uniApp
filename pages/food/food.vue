1bda5a56c76be84cc31c5e5d3b1adbb367dcc4c2
舒航 <673545134@qq.com>
2022-09-28 10:24:29 +0800

    feat：引入ucharts

--- pages/food/food.vue
+++ pages/food/food.vue
@@ -1,22 +1,43 @@
 <template>
-	<view>
-		
-	</view>
+  <view class="charts-box">
+    <qiun-data-charts type="column" :chartData="chartData" />
+  </view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+<script setup>
+import { onReady } from '@dcloudio/uni-app'
+import { ref, reactive, toRefs } from 'vue'
+
+let chartData = ref({})
+onReady(() =>{
+	getServerData()
+})
+const getServerData = () => {
+      //模拟从服务器获取数据时的延时
+			setTimeout(() => {
+				let res = {
+				    categories: ["2016","2017","2018","2019","2020","2021"],
+				    series: [
+				      {
+				        name: "目标值",
+				        data: [35,36,31,33,13,34]
+				      },
+				      {
+				        name: "完成量",
+				        data: [18,27,21,24,6,28]
+				      }
+				    ]
+				  };
+			chartData.value = res
+			console.log(chartData);
+			},500)
+    }
+
 </script>
 
-<style>
-
+<style lang="scss">
+  .charts-box {
+    width: 100%;
+    height: 500rpx;
+  }
 </style>
